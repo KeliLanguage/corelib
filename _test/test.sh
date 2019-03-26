25c4c7b04a4351ceb5121c8a222849fe0e4557e5
Wong Jia Hau <hou32hou@gmail.com>
2019-03-26 10:21:48 +0800

    updated test script

--- _test/test.sh
+++ _test/test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "Testing Boolean.keli"
 keli run './test_Boolean.keli' -l
+
+echo "Testing List.keli"
 keli run './test_List.keli'    -l
+
+echo "Testing Math.keli"
 keli run './test_Math.keli'    -l
+
+echo "Testing String.keli"
 keli run './test_String.keli'  -l
